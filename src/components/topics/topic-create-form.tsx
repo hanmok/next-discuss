76cd062acaefc56e7b3d87d1c953105ecad87d30
hanmok <dmammmm@naver.com>
2024-06-19 04:06:11 +0900

    handle topic validation error

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,6 +1,6 @@
-// import {
-// 	Input
-// }
+"use client";
+
+import { useFormState } from "react-dom";
 
 import {
     Button,
@@ -11,14 +11,19 @@ import {
     Textarea,
 } from "@nextui-org/react";
 import * as actions from "@/actions";
+
 export default function TopicCreateForm() {
+    const [formState, action] = useFormState(actions.createTopic, {
+        errors: {},
+    });
+
     return (
         <Popover placement="left">
             <PopoverTrigger>
                 <Button color="primary">Create a Topic</Button>
             </PopoverTrigger>
             <PopoverContent>
-                <form action={actions.createTopic}>
+                <form action={action}>
                     <div className="flex flex-col gap-4 p-4 w-80">
                         <h3 className="text-lg">Create a Topic</h3>
                         <Input
@@ -26,12 +31,21 @@ export default function TopicCreateForm() {
                             label="Name"
                             labelPlacement="outside"
                             placeholder="Name"
-                        ></Input>
+                            isInvalid={!!formState.errors.name}
+                            errorMessage={formState.errors.name?.join(", ")}
+                        />
+                        {/* <div className="bg-red-400">
+                            {formState.errors.name?.join(", ")}
+                        </div> */}
                         <Textarea
                             name="description"
                             label="Description"
                             labelPlacement="outside"
                             placeholder="Describe your topic"
+                            isInvalid={!!formState.errors.description}
+                            errorMessage={formState.errors.description?.join(
+                                ", "
+                            )}
                         />
                         <Button type="submit">Submit</Button>
                     </div>
